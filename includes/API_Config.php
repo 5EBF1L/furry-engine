ddc1904145e062f24d04f22ac1c2704fffdf4683
5EBF1L <sebastien.filere@protonmail.com>
2022-01-19 15:35:40 +0100

    New code

--- includes/API_Config.php
+++ includes/API_Config.php
@@ -13,6 +13,7 @@ class API_Config {
     =========================================================
     */
 
+    
     protected function db($dataBase = false)
     {
         $host = 'localhost';        //myHostAdress
