b7d6ef68c3ff1d83885edd4e685c4dd4d42ac5ec
5EBF1L <sebastien.filere@protonmail.com>
2022-01-19 15:24:01 +0100

    Creation fichiers

---
+++ includes/Ajax_API.php
@@ -0,0 +1,42 @@
+<?php
+
+require(__DIR__ . '/includes/TokenController');
+
+class Ajax_API extends API_Config {
+
+    public function __construct()
+    {
+		$this->checkToken();
+        
+    }
+	
+	
+	private checkToken(){
+		$token = new TokenController();
+        if (token->init()) {
+            echo $this->filterRequest($_GET);
+        } else {
+            echo json_encode(['data' => null, 'error' => 'Bad token']);
+        }
+	}
+
+    private function filterRequest($data)
+    {
+        //comment
+        switch ($data['table']) {
+
+            case "articles" :
+                return json_encode(['data' => $this->getOneFrom($data['table'], $data['data']), 'error' => null]);
+                break;
+
+
+            case "users"
+                return json_encode(['data' => $this->getOneFrom($data['table'], $data['data']), 'error' => null]);
+                break;
+
+            default : 
+                return json_encode(['data' => null, 'error' => 'Bad table']);
+
+        }
+
+}
---
+++ includes/TokenController.php
@@ -0,0 +1,24 @@
+<?php
+
+class TokenController{
+	
+	// Fonction initiation
+	public function init():boolean {
+
+        if (!empty($_GET)) {
+
+            if (isset($_GET['token']) && $_GET['token'] == '5678') {
+
+
+				return true;
+               
+            } else {
+				return false;;;;
+             
+            }
+        }
+
+    }
+	
+    
+}
